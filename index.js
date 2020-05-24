0aeed66f7ee3336996bbba449ee3a6eb071d1124
rvq <rimacvaldez@gmx.de>
2020-05-24 14:44:16 +0200

    Added middleware to check app version.

--- index.js
+++ index.js
@@ -8,18 +8,53 @@ var path = require('path');
 var fs = require('fs');
 var data = JSON.parse(fs.readFileSync(path.join(__dirname, "dict-minified.json") , 'utf8'));
 
-port = 10000
-textParser = bodyParser.text({ type: 'text/html' })
+const port = 10000
+const textParser = bodyParser.text({ type: 'text/html' })
 
 app.use(bodyParser.json({ type: 'application/json' }))
 
-// POST /login gets urlencoded bodies
+const versionHandler = function (req, res, next) {
+  if (req !== null )
+  {
+    if(req.hasOwnProperty('body') && req.body !== null)
+    {
+      if(req.headers.hasOwnProperty('app-version'))
+      {
+        next()
+      }
+      else{
+        let status = 400
+        res.status(status).send(http.STATUS_CODES[status])
+      }
+    }
+  }
+
+}
+
+app.use(versionHandler)
+
 app.post('/api/imagename', textParser, function (req, res) {
-  res.send('welcome, ' + data[req.body.hanzi].fileName)
+  if (req.body.hasOwnProperty('hanzi') && req.body.hanzi !== null)
+  {
+      let hanzi = req.body.hanzi
+      if(data.hasOwnProperty(hanzi))
+      {
+        if(data[hanzi].hasOwnProperty('fileName') && data[hanzi].fileName !== null){
+
+          res.send(JSON.stringify({"fileName" : data[hanzi].fileName}))
+          return
+        }
+      }
+  }
+  res.send(JSON.stringify({"fileName": "Not Found"}) )
+})
+
+app.get('/api/adfrequence',  function (req, res) {
+    res.send(JSON.stringify({"frequence": 12}))
 })
 
 
 http.createServer(app)
 .listen(port, function () {
-  console.log(`Example app listening on port ${port}! Go to http://localhost:${port}/`)
+  console.log(`api is running on port ${port}! Go to http://localhost:${port}/`)
 })
